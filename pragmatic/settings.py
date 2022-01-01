71bfe1c1627008abc196666fb875b000e4888f9e
이대호 <idaeho@idaehos-MacBook-Pro.local>
2022-01-01 20:54:47 +0900

    Template that let connet python with html

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -70,7 +70,8 @@ ROOT_URLCONF = 'pragmatic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #Connect DIR with Templates for html
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
