05372504f5f0308edc3fa63057b7bd4206a7dcdc
이대호 <idaeho@idaehos-MacBook-Pro.local>
2022-01-07 09:48:04 +0900

    Loading data from input Tag and Saving at Database

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render
 
 # python manage.py startapp accountapp
 # python manage.py runserver
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/index.html', context={'text' : 'POST METHOD!'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/index.html', context={'hello_world_output' : new_hello_world})
     else:
         return render(request, 'accountapp/index.html', context={'text': 'GET METHOD!'})
