67aff94d501ce2776a08f73ef0fe770aca898393
이대호 <idaeho@idaehos-MacBook-Pro.local>
2022-01-09 01:04:14 +0900

    Login and Logout based on LoginView, LogoutView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -6,6 +7,11 @@ app_name = "accountapp"
 urlpatterns = [
     #Function View
     path('hello_world/', hello_world, name='hello_world'),
+
+    #Class View Provided by django
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     #Class View -> as_view
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
