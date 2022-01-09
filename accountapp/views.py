4b1b4b9a6131beace24f50f2c640eb2c5f776d4f
이대호 <idaeho@idaehos-MacBook-Pro.local>
2022-01-09 16:47:59 +0900

    Delete Account based on DeleteView

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render
 # python manage.py startapp accountapp
 # python manage.py runserver
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -45,3 +45,8 @@ class AccountUpdateView(CreateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
