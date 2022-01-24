49331fe032062ae4637b1c555c0e273b0526e17e
이대호 <idaeho@idaehos-MBP.lan>
2022-01-25 03:58:24 +0900

    make profileapp

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -1,9 +1,9 @@
-from profile import Profile
-
 from django.forms import ModelForm
+
+from profileapp.models import Profile
 
 
 class ProfileCreationForm(ModelForm):
     class Meta:
         model = Profile
-        field = ['image', 'nickname', 'message']
+        fields = ['image', 'nickname', 'message']
--- profileapp/models.py
+++ profileapp/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -9,4 +10,3 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
-
